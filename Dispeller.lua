6592d1047032593dd922b4df8fb394b1e2d363be
LumielGR <lumielgr@gmail.com>
2018-09-15 18:56:22 +0300

    Added more events, UI updates.

--- Dispeller.lua
+++ Dispeller.lua
@@ -4,70 +4,82 @@
 --
 
 local version = GetAddOnMetadata("Dispeller", "Version")
+DispellerFrame = nil
 
-function Dispeller(self)
+function Dispeller_Load(self)
     print("Loaded: " .. version)
     local _, playerClass = UnitClass("player")
 
+    DispellerFrame = self
     self:RegisterEvent("ADDON_LOADED")
     self:RegisterEvent("UNIT_AURA")
-    self:RegisterForDrag("button")
-    self.Locked = false
-    self:Show()
+    self:RegisterEvent("PLAYER_DEAD")
+    self:RegisterEvent("UNIT_TARGET")
+    self:RegisterEvent("PLAYER_TARGET_CHANGED")
+    self.Locked = true
+    self:Hide()
     SLASH_DISPELLER1 = "/dispeller"
     SlashCmdList["DISPELLER"] = Dispeller_cmdHandle
     DispellerTitle.Text:SetText("Dispeller v" .. version)
 end
 
 function Dispeller_handleEvent(self, event, ...)
-    local args = {...}
-    if (event == "UNIT_AURA") then
+    local args = {... }
+    if (event == "ADDON_LOADED") then
+        if (args[1] == "Dispeller") then
+            -- Todo: Future global options
+            if (type(DispellerSettings) ~= "table") then
+                DispellerSettings =  { }
+            end
+
+            -- self:ClearAllPoints()
+            -- self:SetPoint("CENTER", 0, 0)
+            -- self:SetPoint("BOTTOMLEFT", DispellerSettings.positionX, DispellerSettings.positionY)
+            DispellerSettings.Debug = true
+        end
+    elseif (event == "PLAYER_TARGET_CHANGED")  then
+        Dispeller_Update()
+    elseif (event == "UNIT_TARGET" and args[1] == "player") then
+        Dispeller_Update()
+    elseif (event == "PLAYER_DEAD") then
+        Dispeller_Update()
+    elseif (event == "UNIT_AURA") then
         if (args[1] ~= "target" and DispellerSettings.Debug == false) then do return end end
         logdebug(event .. " on " .. args[1])
-        local stealable = {}
-        for i = 1,40 do
-            local name, icon, count, debuffType, duration, expirationTime, unitCaster, canStealOrPurge = UnitAura("target", i, "HELPFUL")
-            if (name == nil) then break else
-                if (canStealOrPurge) then
-                    table.insert(stealable, name)
-                    logdebug(name .. " stealable: " .. tostring(canStealOrPurge))
-                end
-            end
-        end
-        if (DispellerSettings.Debug) then
-            stealable = {"Buff1", "Buff2", "Buff3", "Buff4", "Buff5", "Buff6", "Buff7", "Buff8", "Buff9" }
-        end
+        Dispeller_Update()
+    end
+end
 
-        if (#stealable < 1) then
-            if not DispellerSettings.Debug then
-                self:Hide()
+function Dispeller_Update()
+    local stealable = {}
+    for i = 1,40 do
+        local name, icon, count, debuffType, duration, expirationTime, unitCaster, canStealOrPurge = UnitAura("target", i, "HELPFUL")
+        if (name == nil) then break else
+            if (canStealOrPurge) then
+                table.insert(stealable, name)
+                logdebug(name .. " stealable: " .. tostring(canStealOrPurge))
             end
-        else
-            local height = 20 * #stealable
-            stealable = table.concat(stealable, "\n")
---            DispellerBuffs:SetHeight(height)
-            DispellerBuffs.Text:SetText(stealable)
-            self:SetHeight(height)
-            DispellerBuffs:SetHeight(height)
---            DispellerBuffs:ClearAllPoints()
---            DispellerBuffs:SetPoint("BOTTOMLEFT", 0, -22)
---            DispellerBuffs:Show()
-            self:Show()
         end
-    elseif (event == "ADDON_LOADED") then
-        if (args[1] == "Dispeller") then
-            if (type(DispellerSettings) ~= "table") then
-                DispellerSettings =  {
-                    positionX = 0,
-                    positionY = 0
-                } end
---            if (DispellerSettings.positionX == nil) then DispellerSettings.positionX = 0 end
---            if (DispellerSettings.positionY == nil) then DispellerSettings.positionY = 0 end
-            self:ClearAllPoints()
-            self:SetPoint("CENTER", 0, 0)
-            self:SetPoint("BOTTOMLEFT", DispellerSettings.positionX, DispellerSettings.positionY)
-            DispellerSettings.Debug = false
+    end
+    if (DispellerSettings.Debug) then
+        stealable = {"Buff1", "Buff2", "Buff3", "Buff4", "Buff5", "Buff6", "Buff7", "Buff8", "Buff9" }
+    end
+
+    if (#stealable < 1) then
+        if not DispellerSettings.Debug then
+            DispellerFrame:Hide()
         end
+    else
+        local height = 12 * #stealable
+        stealable = table.concat(stealable, "\n")
+        --            DispellerBuffs:SetHeight(height)
+        DispellerBuffs.Text:SetText(stealable)
+--        DispellerFrame:SetHeight(height)
+        DispellerBuffs:SetHeight(height)
+--        DispellerBuffs:ClearAllPoints()
+--        DispellerBuffs:SetPoint("BOTTOMLEFT", 0, -22)
+        DispellerBuffs:Show()
+        DispellerFrame:Show()
     end
 end
 
